d76d72870019469840fcbea4e7c9259ddc0fba49
Marcelo Augusto <marcelo513.ma@gmail.com>
2024-09-13 14:11:17 -0300

    Validação_Cadastro

--- flutter_application_1/lib/field_form.dart
+++ flutter_application_1/lib/field_form.dart
@@ -8,13 +8,15 @@ class FieldForm extends StatelessWidget {
   TextEditingController controller;
   bool? isForm = true;
   bool isEmail = false;
-  FieldForm(
-      {required this.label,
-      required this.isPassword,
-      required this.controller,
-      this.isForm,
-      required this.isEmail,
-      super.key});
+
+  FieldForm({
+    required this.label,
+    required this.isPassword,
+    required this.controller,
+    this.isForm,
+    required this.isEmail,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -28,12 +30,44 @@ class FieldForm extends StatelessWidget {
         labelText: label,
       ),
       validator: (value) {
-        if (value!.length < 5){
-          return 'Digite Pelo Menos 5 Caracteres';
+        // Verifica se o campo está vazio ou nulo
+        if (value == null || value.isEmpty) {
+          return 'Este campo não pode ficar vazio';
         }
-        if (this.isEmail && !value.contains("@")) {
-          return 'Digite um Email Válido';
+
+        // Validação de e-mail
+        if (this.isEmail) {
+          if (!value.contains("@") || !value.contains(".")) {
+            return 'Digite um Email Válido com domínio';
+          }
+        } 
+        // Validação de senha
+        else if (isPassword) {
+          if (value.length < 8) {
+            return 'A senha deve ter no mínimo 8 caracteres';
+          }
+
+          bool hasUpperCase = value.contains(RegExp(r'[A-Z]'));
+          bool hasLowerCase = value.contains(RegExp(r'[a-z]'));
+          bool hasDigits = value.contains(RegExp(r'[0-9]'));
+          bool hasSpecialCharacters = value.contains(RegExp(r'[!@#\$&*~]'));
+
+          if (!hasUpperCase) {
+            return 'A senha deve conter pelo menos uma letra maiúscula';
+          }
+          if (!hasLowerCase) {
+            return 'A senha deve conter pelo menos uma letra minúscula';
+          }
+          if (!hasDigits) {
+            return 'A senha deve conter pelo menos um número';
+          }
+          if (!hasSpecialCharacters) {
+            return 'A senha deve conter pelo menos um caractere especial (!@#\$&*~)';
+          }
         }
+
+        // Se passar em todas as validações, retorna null (nenhum erro)
+        return null;
       },
     );
   }
